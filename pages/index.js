cb43bec5df0a59afe7a2ddec854a9a89bd72cc34
Tom Davidson <tom@davidson.me.uk>
2020-09-01 14:21:06 +0100

    basic diagram colouring and clean-up

--- pages/index.js
+++ pages/index.js
@@ -2,53 +2,62 @@ import MainLayout from '../layouts/main';
 import { Graph } from "react-d3-graph";
 import NoSSR from "../components/nossr";
 
+// See https://danielcaldas.github.io/react-d3-graph/docs/ for options
 const graphConfig = {
 	directed: true,
-    nodeHighlightBehavior: true,
+	nodeHighlightBehavior: true,
+	d3: {
+		linkLength: 275,
+	},
     node: {
+        size: 1500,
         color: "lightgreen",
-        // size: 120,
-        highlightStrokeColor: "blue",
+		highlightStrokeColor: "blue",
+		fontSize: 18,
+		highlightFontSize: 18,
     },
     link: {
-        highlightColor: "lightblue",
+		renderLabel: true,
+        color: "lightgreen",
+		highlightColor: "lightblue",
+		semanticStrokeWidth: true,
     },
 };
 const onClickGraph = function() {
-    console.log(`Clicked the graph background`);
+    // console.log(`Clicked the graph background`);
 };
 const onClickNode = function(nodeId) {
-    console.log(`Clicked node ${nodeId}`);
+    // console.log(`Clicked node ${nodeId}`);
 };
 const onDoubleClickNode = function(nodeId) {
-    console.log(`Double clicked node ${nodeId}`);
+    // console.log(`Double clicked node ${nodeId}`);
 };
 const onRightClickNode = function(event, nodeId) {
-    console.log(`Right clicked node ${nodeId}`);
+    // console.log(`Right clicked node ${nodeId}`);
 };
 const onMouseOverNode = function(nodeId) {
-    console.log(`Mouse over node ${nodeId}`);
+    // console.log(`Mouse over node ${nodeId}`);
 };
 const onMouseOutNode = function(nodeId) {
-    console.log(`Mouse out node ${nodeId}`);
+    // console.log(`Mouse out node ${nodeId}`);
 };
 const onClickLink = function(source, target) {
-    console.log(`Clicked link between ${source} and ${target}`);
+    // console.log(`Clicked link between ${source} and ${target}`);
 };
 const onRightClickLink = function(event, source, target) {
-    console.log(`Right clicked link between ${source} and ${target}`);
+    // console.log(`Right clicked link between ${source} and ${target}`);
 };
 const onMouseOverLink = function(source, target) {
-    console.log(`Mouse over in link between ${source} and ${target}`);
+    // console.log(`Mouse over in link between ${source} and ${target}`);
 };
 const onMouseOutLink = function(source, target) {
-    console.log(`Mouse out link between ${source} and ${target}`);
+    // console.log(`Mouse out link between ${source} and ${target}`);
 };
 const onNodePositionChange = function(nodeId, x, y) {
-    console.log(`Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`);
+    // console.log(`Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`);
 };
 const onZoomChange = (prevZoom, newZoom) => {
-	console.log(`Zoom changed from ${prevZoom} to ${newZoom}`);
+	// console.log(`Zoom changed from ${prevZoom} to ${newZoom}`);
 };
 
 function HomePage({ mapData, env }) {
@@ -58,7 +67,6 @@ function HomePage({ mapData, env }) {
 				<p>Nodes: {mapData.nodes.length}, Links: {mapData.links.length}</p>
 				<p>Server-side env data: {env}</p>
 				<Graph
-					// ref="graph"
 					id="graph"
 					data={mapData}
 					config={graphConfig}
